dea6aecc527bf38bb6899c4a0e486e0ff3f3fd9a
Soham <sohammondal39@gmail.com>
2020-12-04 20:12:35 +0530

    everything_up

--- Normal_arindam/script_normal.sh
+++ Normal_arindam/script_normal.sh
@@ -2,10 +2,6 @@
 
 sudo chmod +x rpi_button_push_normal.py
 
-if [ -d ./log ]; then
-  # Take action if log DIR exists. #
-  echo "echo creating log DIR ..."
-  mkdir -p log
-fi
+ls ./log 2>/dev/null||mkdir ./log
 
 python3 rpi_button_push_normal.py | tee ./log/out.txt
